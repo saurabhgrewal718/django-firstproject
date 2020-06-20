e707b354f78e9431959fd9e390407af9aaff4960
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-06-20 16:50:17 +0530

    till the django template language all done!

--- calc/views.py
+++ calc/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(requests):
-    return HttpResponse("saurabh here")
+def home(request):
+    return render(request,'home.html',{'name':'manto'})
